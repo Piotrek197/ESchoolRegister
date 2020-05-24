f5cdd2f55b76282354888f59a10ca6223b3ba0b8
Piotrek197 <65012447+Piotrek197@users.noreply.github.com>
2020-05-24 19:28:26 +0200

    Update requestReset.php

--- requestReset.php
+++ requestReset.php
@@ -35,8 +35,8 @@
             $mail->isSMTP();                                            // Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'testtest05516@gmail.com';                     // SMTP username
-            $mail->Password   = 'imncotrwyfkutvnv';                                // SMTP password
+            $mail->Username   = '**********************';                     // SMTP username
+            $mail->Password   = '***********';                                // SMTP password
             $mail->SMTPSecure = 'ssl';//PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         
